94beb11611fa386be1da299fb027d56a6ff53cd1
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-16 10:40:15 +0200

    add Examples004_Div

---
+++ Examples004_Div/Program.cs
@@ -0,0 +1,3 @@
+﻿double numberA = 12;                        //double - вещественное число (не целое число)
+double numberB = 5;                         //double - вещественное число (не целое число)
+Console.WriteLine(numberA / numberB);       //WriteLine - вывести что-то в окно терминала, в данном случае текст
