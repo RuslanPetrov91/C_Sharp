9f0c2b4a64242886aaa05f6952d4d349ac2c8ed5
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-16 19:57:00 +0200

    Task20_same

---
+++ Task20_same/Program.cs
@@ -0,0 +1,39 @@
+﻿System.Console.WriteLine("Укажите четверть ");
+
+static void DeterminadeDiapozon(int GetQuorterNumber)
+{
+    switch (GetQuorterNumber)
+    {
+        case 1:
+            System.Console.WriteLine("Первая четверть, диапозон x равен 1 - до бесконечности, y равен 1 - до бесконечности ");
+            break;
+        case 2:
+            System.Console.WriteLine(" Вторая четверть, диапозон x равен -1 - (-до бесконечности), y равен 1 - до бесконечности ");
+            break;
+        case 3:
+            System.Console.WriteLine("Третья четверть, диапозон x равен -1 - (-до бесконечности), y равен -1 - (-до бесконечности) ");
+            break;
+        case 4:
+            System.Console.WriteLine("Четвертая четверть, диапозон x равен 1 - до бесконечности, y равен -1 - (-до бесконечности) ");
+            break;
+    }
+   
+}
+string quorter = Console.ReadLine();
+int GetQuorterNumber = Convert.ToInt32(quorter);
+
+bool ValidateCoords(int GetQuorterNumber)
+{
+    if (GetQuorterNumber > 0 && GetQuorterNumber <= 4)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Такой четверти нет ");
+    return false;
+}
+
+if (ValidateCoords(GetQuorterNumber))
+{
+    DeterminadeDiapozon(GetQuorterNumber);
+
+}
