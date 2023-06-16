322227844a5530ba995da4e118c15a65b2888193
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-16 10:37:55 +0200

    add Examples003_Sum

---
+++ Examples003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10);     //1 2 3 4 ... 9; new Random().Next(1,10) - случайное целое число
+Console.WriteLine(numberA);                // Write - вывести что-то в окно терминала, в данном случае текст
+int numberB = new Random().Next(1,10);     //1 2 3 4 ... 9; new Random().Next(1,10) - случайное целое число
+Console.WriteLine(numberB);                // Write - вывести что-то в окно терминала, в данном случае текст
+int sum = numberA + numberB;               //int (integer) - целое число   //Промежуточная переменная
+Console.WriteLine(sum);                    // Write - вывести что-то в окно терминала, в данном случае текст
