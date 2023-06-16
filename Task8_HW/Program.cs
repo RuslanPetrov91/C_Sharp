cd678c874a59097744a8382ec5c5189cc34f6018
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-16 15:06:41 +0200

    add Task8_HW

---
+++ Task8_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите число => ");
+string inputedString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString1);
+int number2 = 1;
+int number3 = 2;
+while (number2 <= number1)
+{
+    if (number2 % number3 == 0)
+    {
+        Console.Write($"{number2} ");
+    }
+    number2++;
+}
