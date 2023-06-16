c5d91c78ace90b14c0eb1a21d5e2b89c6eacf177
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-16 14:47:24 +0200

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] week = { "Понедельник", "Вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+
+Console.Write("Введите номер дня недели: ");
+string weekday = Console.ReadLine();                                // Ввод пользователя (строка)
+int dayNumber = Convert.ToInt32(weekday);
+if (dayNumber < 0 || dayNumber > 7)
+{
+    Console.WriteLine("Нет такого дня недели!");
+}
+else
+{
+    Console.WriteLine(week[dayNumber - 1]);
+}
