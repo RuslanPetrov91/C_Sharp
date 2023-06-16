d3e1cc6ea6001b7e7b92764757ff82dd94791057
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-16 14:56:45 +0200

    add Task7_HW

---
+++ Task7_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число => "); 
+string inputedString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString1);   
+int number2 = 2;
+if (number1 % number2 == 0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("Нечетное");
+}
