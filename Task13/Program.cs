dd6399bbc2584c9c08fb666f8206a751b97ef126
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-18 21:28:05 +0200

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновремененно 7 и 23.
+//14 -> нет
+//46 -> нет
+//161 -> да
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое число -> ");
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.WriteLine($"Да");
+}
+else
+{
+    System.Console.WriteLine($"Нет");
+}
