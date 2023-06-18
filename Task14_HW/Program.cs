4bf4ebccd44ba13bbf85c935261bbb406d5ee5df
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-18 22:29:44 +0200

    Task14_HW

---
+++ Task14_HW/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на
+// вход трехзначное число и на выходе показывает вторую
+// цифру этого числа. Не использовать строки для расчета
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите трехзначное число -> ");
+int value = number / 10 % 10;
+
+System.Console.WriteLine($"{value}");
