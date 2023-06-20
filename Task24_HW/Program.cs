0e0676c059e520abc28c73fa3af92140319e8acf
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-20 08:49:32 +0200

    Task24_HW

---
+++ Task24_HW/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты трех точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7) -> 15,84 
+// A (7,-5,0); B (1,-1, 9) -> 11,53
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+double Length (int x, int y, int z)
+{
+   double temp = (x*x) + (y*y) + (z*z);
+   return Math.Sqrt(temp); 
+}
+
+int x1 = ReadInt("Введите координату X первой точки ");
+int y1 = ReadInt("Введите координату Y первой точки ");
+int z1 = ReadInt("Введите координату Z первой точки ");
+int x2 = ReadInt("Введите координату X второй точки ");
+int y2 = ReadInt("Введите координату Y второй точки ");
+int z2 = ReadInt("Введите координату Z второй точки ");
+
+double spatialDiagonal= Length(x1-x2, y1-y2, z1-z2);
+Console.WriteLine($"Расстояние между точками в 3D пространстве равно {spatialDiagonal}");
