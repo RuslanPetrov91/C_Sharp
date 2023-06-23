e622a9a3e0077ce1a1ef74dcc858d47ce954d180
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-23 10:43:53 +0200

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿int lengthnumber(int arg)     // Определяет длинну числа
+{
+    int temporarynunber = arg;
+    int lengthnunber = 0;
+    while(temporarynunber > 0)
+    {
+        temporarynunber /= 10;
+        lengthnunber += 1;
+    }
+    return lengthnunber;
+}
+
+int redin(string tex)
+{
+    Console.WriteLine(tex);
+    string number = Console.ReadLine();
+    int number2 = Convert.ToInt32(number);
+    return number2;
+}
+
+int number2 = redin($"Введите число");
+int c = lengthnumber(number2);
+
+
+
+Console.WriteLine(c);
