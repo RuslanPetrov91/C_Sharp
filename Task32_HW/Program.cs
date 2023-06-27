48f7f8c0308b72b72fd5f7412ac4489c8b3900c8
Ruslan Petrov <ruslan.petrov01031991@gmail.com>
2023-06-27 18:45:09 +0200

    Task32_HW

---
+++ Task32_HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNumber(int arg)                      // Определяет сумму числа массива
+{
+    int sum = 0;
+    while (arg > 0)
+    {
+        sum+=arg % 10;
+        arg = arg /10;
+    }
+    return sum;
+}
+
+int redIn(string text)                      //Ввод данных
+{
+    Console.WriteLine(text);
+    string number = Console.ReadLine();
+    int number2 = Convert.ToInt32(number);
+    return number2;
+}
+
+int number2 = redIn($"Введите число");
+int c = SumNumber(number2);
+
+Console.WriteLine(c);
